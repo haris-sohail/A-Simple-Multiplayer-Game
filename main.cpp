7f2ea98477769054bac8bf4a852c4b2fca16de46
haris-sohail <i210531@nu.edu.pk>
2023-04-01 11:24:08 +0500

    Added a player sprite on the board.

--- main.cpp
+++ main.cpp
@@ -33,8 +33,20 @@ int generateRandom()
     return division;
 }
 
-void createBoard(int n, int boxSize, int boardSizePixels)
+int main()
 {
+    int n = generateRandom(); 
+
+    // generate a n x n board 
+
+    int boxSize = 60;
+
+    // get the size of the window in pixels
+
+    int boardSizePixels = n * 60;
+
+    // creating the board
+    
     // create the textures first
 
     // create the window
@@ -64,19 +76,44 @@ void createBoard(int n, int boxSize, int boardSizePixels)
                 squareShape.setFillColor(Color(192, 192, 180));
             }
 
-            // now draw the squareShape on the window
             window.draw(squareShape);
         }
     }
 
-    // now display the board
+    // create the sprite 
 
-    // display the board
+    // create a texture first
+    Texture spriteTexture;
+
+    Sprite sprite;
+
+    // Failed to load image
+
+    if(!spriteTexture.loadFromFile("assets/player1.png"))
+    {
+        cout << "Error. Could not load sprite" << endl;
+    }
+
+    else
+    {
+        // Successfully loaded image
+
+        sprite.setTexture(spriteTexture);
+    }
+
+    sprite.setScale(3.6, 3.6); // making the sprite twice as bigger
+
+    // draw the sprite
+
+    window.draw(sprite);
+
+    // now display the sprite and the board
     window.display();
 
-    // run the window
+    // display the window
     while (window.isOpen())
     {
+        // user interaction
         Event event;
 
         while (window.pollEvent(event))
@@ -86,21 +123,32 @@ void createBoard(int n, int boxSize, int boardSizePixels)
                 window.close();
             }
         }
+
+        //updates
+
+        // draw
+        window.clear();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                // set the position of a particular box
+                squareShape.setPosition(i * boxSize, j * boxSize); // this function takes x and y coordinates as pixel values
+
+                // bicolor table functionality
+                if((i + j) % 2 == 0)
+                {
+                    squareShape.setFillColor(Color(64, 64, 64));
+                }
+                else
+                {
+                    squareShape.setFillColor(Color(192, 192, 180));
+                }
+
+                window.draw(squareShape);
+            }
+        }
+        window.draw(sprite);
+        window.display();
     }
-}
-
-int main()
-{
-    int n = generateRandom(); 
-
-    // generate a n x n board 
-
-    int boxSize = 60;
-
-    // get the size of the window in pixels
-
-    int boardSizePixels = n * 60;
-
-    // creating the board
-    createBoard(n, boxSize, boardSizePixels);
 }
